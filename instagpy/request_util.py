b88b46c790ad28b0af9d33ee0f095baf12e999c6
Sarabjit Dhiman <isarabjitdhiman@gmail.com>
2023-05-31 20:43:01 +0530

    Add timeout functionality to request_util module.

--- instagpy/request_util.py
+++ instagpy/request_util.py
@@ -2,16 +2,18 @@ import requests
 import bs4
 
 
-def make_request(url, session=None, method=None, max_retries=None, **kwargs):
+def make_request(url, session=None, method=None, max_retries=None, timeout=None, **kwargs):
     if method is None:
         method = "GET"
     if max_retries is None:
         max_retries = 3
     if session is None:
         session = requests.Session()
+    if timeout is None:
+        timeout = 30
     for retry_count, retry in enumerate(range(max_retries), start=1):
         try:
-            response = session.request(method, url, **kwargs)
+            response = session.request(method, url, timeout=timeout, **kwargs)
             response.raise_for_status()
             soup = bs4.BeautifulSoup(response.content, "lxml")
             if "json" in response.headers["Content-Type"]:
